e5f9dab5aa0a56971650a54ec50e75454e7cd95c
Rupendra Dhungana <dhunganarupendra03@gmail.com>
2026-08-19 09:03:57 +0545

    fetch jason data

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -5,20 +5,24 @@ let productRoutes = Router();
 productRoutes
   .route("/") //localhost;8000/product
   .post((req, res, next) => {
-    res.json("create product");
+    res.json("create post product");
     // res.json(req.body);
-    console.log(req.body); // to print body
-    // console.log(req.query); // to print the input given in postman with paras query method
+    // console.log(req.body); // to print body
+    // console.log(req.params);
     console.log("this gandu post request");
+    console.log(req.query); // to print the input given in postman with paras query method
   })
   .get((req, res, next) => {
     res.json("get product");
+    console.log(req.body);
   })
   .put((req, res, next) => {
     res.json("put product");
+    res.json("create put product");
   })
   .delete((req, res, next) => {
     res.json("delet product");
+    res.json("create delete product");
   });
 
 export default productRoutes;
